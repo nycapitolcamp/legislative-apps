dbef1eddf01aa894a8d3eb20f264ab7f1fbd96ff
kotfic <kotfic@gmail.com>
2012-11-15 18:59:46 -0500

    adding some general notes and documentation

--- backbonetry1/js/app.js
+++ backbonetry1/js/app.js
@@ -1,53 +1,34 @@
-﻿//demo data
+﻿/**
+  *  Notes - HOORAY
+  * 
+  *  Backbone does its best to enforce a seperation between data models and how that data is presented
+  *  Those familier with the MVC patter will recognize some similarities. This can be particularly confusing
+  *  because this is all implemented in Javascript which is often refered to as "Frontend" and therefore
+  *  presentation oriented,  as opposed to "Backend"  which traditionally deals with models, collections
+  *  (ie. arrays, iterables etc) and other non-presentation related "STUFF."
+  * 
+  *  Backbone breaks this worldview a little by providing another layer of abstraction. Now on the traditional 
+  *  "Frontend"  we also have a "Backend."  Websites for the most part are HTML affairs,  browser requests html
+  *  server delivers html,  browser renders html. In the world where backbone lives,  The browser requests javascript
+  *  and data in the form of JSON.  This is great beacuse if the data is on your client then you can play around
+  *  with it visually without waiting for the server to get back to you.  BUT  when you move the data from the server
+  *  to the client,  you also have to move the stuff that processes that data.  This is where backbone models and
+  *  collections come in.  These objects hold the data,  and the functions that operate on that data.  Backbone
+  *  views on the other hand take models and collections as arguments,  then they present or 'render' things to
+  *  the browser DOM.  Models/Collections and Views may be seperate,  but they talk quite a bit to eachother. This
+  *  is managed throug events.  i'll point some of these out in further documentation.
+  *
+  *  Chris - 11/15/2012
+  **/
 
-var realBills = [
-	{
-		"year":"2011",
-		"senateBillNo":"S607-2011",
-		"title":"Relates to the definition of alternate energy production facilities",
-		"lawSection":"Public Service Law",
-		"sameAs":"A3536",
-		"previousVersions":["S8310-2009"],
-		"sponsor":{"fullname":"MAZIARZ"},
-		"coSponsors":null,
-		"multiSponsors":null,
-		"summary":"Adds lithium ion energy batteries to the definition of alternate energy production facilities.",
-		"currentCommittee":null,
-		"actions":[null, null, null],
-		"fulltext": "A really long string",
-		"memo": "A much shorter string",
-		"law":"Amd S2, Pub Serv L ",
-		"votes":[null, null, null]
-	},
-	{
-		"year":"2011",
-		"senateBillNo":"S607A-2011A",
-		"title":"Relates to the definition of alternate energy production facilities",
-		"lawSection":"Public Service Law",
-		"sameAs":"A3536",
-		"previousVersions":["S8310-2009"],
-		"sponsor":{"fullname":"MAZIARZ"},
-		"coSponsors":null,
-		"multiSponsors":null,
-		"summary":"Adds lithium ion energy batteries to the definition aaaa of alternate energy production facilities.",
-		"currentCommittee":null,
-		"actions":[null, null, null],
-		"fulltext": "A really long string",
-		"memo": "A much shorter string",
-		"law":"Amd S2, Pub Serv L ",
-		"votes":[null, null, null]
-	}
-]
-
-
-
-var bills;
+/*
+ *  Backend Models and Collections
+ */
 
 //define BillTimeline collection
 var BillTimeline = Backbone.Collection.extend({
     model: Bill,
 });
-
 
 //define product model
 var Bill = Backbone.Model.extend({
@@ -107,21 +88,24 @@ var Bill = Backbone.Model.extend({
 });
 
 
+
+/*
+ *  Frontend Views
+ */
+
+
 //define individual contact view
 var BillView = Backbone.View.extend({
-    tagName: "article",
-    className: "contact-container",
     template: $("#billTemplate").html(),
-
+    
     initialize: function(){
 	this.render = _.bind(this.render, this);
 	this.model.bind("change", this.render);
     },
-
+    
     render: function () {
 	var tmpl = _.template(this.template);
 	
-
 	$(this.el).append(tmpl(this.model.toJSON()));
 	return this;
     }
@@ -157,13 +141,20 @@ var BillTimelineView = Backbone.View.extend({
     }
 });
 
-//create instance of master view
-//var BillTimeline = new BillTimelineView();
+/*
+ *  This is the code that gets the whole ball rolling.
+ */
+
 jQuery(document).ready(function(){
-    var billID = $(this).find('[name=bill-id]').val();
-    if(billID){
+    // pull the billID from the input element
+    // moving forward we'll probably want to move this into
+    // a function which can be set on the form 'submit' button
+    // instead of actually submitting the form we catch the
+    // submit event and run this code instead.
+
+    if( $('[name=bill-id]').val() ){
 	var b = new Bill();
-	b.set("id",billID);
+	b.set("id", $('[name=bill-id]').val());
 
 	var billView = new BillView({
 	    model : b,
